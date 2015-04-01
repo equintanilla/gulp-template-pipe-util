44c57040209536bd957c4b2432784e2e991099ec
equintanilla <edgardo@icracked.com>
2015-04-01 10:45:01 -0700

    implemented pipeBuilder

--- index.js
+++ index.js
@@ -59,3 +59,81 @@ exports.lazyPipeFactory = function (func, eventListeners) {
 
     }
 };
+
+var calculateFinalListeners = function (listeners, defaultFactories, pipeName) {
+    var errorFunction;
+    var finishFunction;
+    if (defaultFactories) {
+        errorFunction = defaultFactories.errorFunctionFactory({pipeStep: pipeName});
+        finishFunction = defaultFactories.onFinishFunctionFactory({pipeStep: pipeName});
+    }
+
+
+    var finalListeners = {
+        error: [],
+        finish: []
+    };
+
+    if (errorFunction) {
+        finalListeners.error.push(errorFunction);
+    }
+    if (finishFunction) {
+        finalListeners.finish.push(finishFunction);
+    }
+
+
+    for (var event in listeners) {
+        var currentListeners = listeners[event];
+
+        if (typeof currentListeners === 'function') {
+            if (util.isArray(finalListeners[event])) {
+                finalListeners[event].push(currentListeners);
+            } else if (!finalListeners[event]) {
+                finalListeners[event] = currentListeners;
+            }
+        }
+
+        if (util.isArray(currentListeners)) {
+            if (util.isArray(finalListeners[event])) {
+                finalListeners[event] = finalListeners[event].concat(currentListeners);
+            } else if (!finalListeners[event]) {
+                finalListeners[event] = currentListeners;
+            }
+        }
+
+    }
+    console.log('final listeners');
+    console.log(finalListeners);
+    return finalListeners;
+};
+
+exports.pipeBuilder = function (pipesArray, skipDefaultListeners) {
+    var defaultFactories;
+    if (!skipDefaultListeners) {
+        defaultFactories = exports.getDefaultFactories();
+        defaultFactories.setStartTimeToNow();
+    }
+
+    var lazyPipesMap = {};
+    for (var i = 0; i < pipesArray.length; i++) {
+        var currentPipeInformation = pipesArray[i];
+        var pipeName = currentPipeInformation.name;
+        var pipeFunction = currentPipeInformation.func;
+        var pipeListeners = currentPipeInformation.listeners;
+
+        lazyPipesMap[pipeName] = exports.lazyPipeFactory(pipeFunction, calculateFinalListeners(pipeListeners, defaultFactories, pipeName));
+    }
+
+    var executingPipe;
+    for (var i = 0; i < pipesArray.length; i++) {
+        var currentPipeName = pipesArray[i].name;
+        var currentLazyPipe = lazyPipesMap[currentPipeName];
+        if (i === 0) {
+            executingPipe = currentLazyPipe();
+        } else {
+            executingPipe = executingPipe.pipe(currentLazyPipe());
+        }
+    }
+    return executingPipe;
+
+};
---
+++ spec/pipeBuilderSpec.js
@@ -0,0 +1,127 @@
+var gulp = require('gulp');
+var gulpPipeUtils = require('../');
+
+describe("pipe Builder", function () {
+
+    it('should build task successfully', function (done) {
+
+        var taskDescription = [
+            {
+                name: 'src', func: function () {
+                return gulp.src('./test/fixtures/dummy');
+            }, listeners: {
+                finish: [function () {
+                    console.log('first finish callback');
+                },
+                    function () {
+                        console.log('second finish callback');
+                    }]
+            }
+            },
+            {
+                name: 'dest', func: function () {
+                return gulp.dest('./example');
+            }, listeners: {
+                finish: function () {
+                    console.log('finish callback');
+                    done();
+                }
+            }
+            }
+
+        ];
+
+        gulpPipeUtils.pipeBuilder(taskDescription);
+
+
+    });
+
+    it('should build task successfully on skipping default listeners', function (done) {
+
+        var taskDescription = [
+            {
+                name: 'src', func: function () {
+                return gulp.src('./test/fixtures/dummy');
+            }, listeners: {
+                finish: [function () {
+                    console.log('first finish callback');
+                },
+                    function () {
+                        console.log('second finish callback');
+                    }]
+            }
+            },
+            {
+                name: 'dest', func: function () {
+                return gulp.dest('./example');
+            }, listeners: {
+                finish: function () {
+                    console.log('finish callback');
+                    done();
+                }
+            }
+            }
+
+        ];
+
+        gulpPipeUtils.pipeBuilder(taskDescription, true);
+
+    });
+    it('should build task successfully on no listener', function (done) {
+
+        var taskDescription = [
+            {
+                name: 'src', func: function () {
+                return gulp.src('./test/fixtures/dummy');
+            }
+            },
+
+            {
+                name: 'dest', func: function () {
+                return gulp.dest('./example');
+            }
+            }
+
+        ];
+
+        gulpPipeUtils.pipeBuilder(taskDescription, true)
+            .on('finish',function(){
+                done();
+            });
+
+
+
+    });
+
+    it('should build task successfully', function (done) {
+
+        var errorCallbacksCalled = 0;
+        var expectedErrorsCallled = 2;
+        var taskDescription = [
+            {
+                name: 'src', func: function () {
+                return gulp.src('./test/fixtures/dummy');
+            }, listeners: {
+                error: [function () {
+                    console.log('first error callback');
+                    errorCallbacksCalled++;
+                },
+                    function () {
+                        console.log('second finish callback');
+                        errorCallbacksCalled++;
+
+                    }]
+            }
+            }
+
+        ];
+
+        var pipe = gulpPipeUtils.pipeBuilder(taskDescription)
+            .on('error',function(){
+                expect(errorCallbacksCalled).toBe(expectedErrorsCallled);
+                done();
+            });
+        pipe.emit('error',new Error('something weng wrong'));
+
+    });
+});
